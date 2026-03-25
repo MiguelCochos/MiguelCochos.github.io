6763dc87366d29c80a2ad8dcafe7380c1337e666
MiguelCochos <miguelcochos8@gmail.com>
2026-03-24 20:26:16 -0400

    Update

--- scripts.js
+++ scripts.js
@@ -17,3 +17,40 @@ document.addEventListener('DOMContentLoaded', () => {
     const revealElements = document.querySelectorAll('.reveal');
     revealElements.forEach(el => observer.observe(el));
 });
+
+/* Menu desplegable */
+
+let menuButton = document.getElementById('menuButton');
+let navLinks = document.getElementById('navLinks');
+
+menuButton.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    if (!navLinks.classList.contains('active')) {
+        setTimeout(() => {
+            navLinks.style.display = 'none';
+        }, 500);
+    } else {
+        navLinks.style.display = 'flex';
+    }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    if (window.innerWidth < 768) {
+        navLinks.style.display = 'none';
+    }
+});
+
+/* Resaltar enlace activo */
+
+const links = document.querySelectorAll('.navButtton');
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        links.forEach(l => l.classList.remove('linkOn'));
+        link.classList.add('linkOn');
+        navLinks.classList.remove('active');
+        setTimeout(() => {
+            navLinks.style.display = 'none';
+        }, 500);
+    });
+});
